544377df9dafad31f33be06878c8e5cc859a0a6b
AnorVen <efremovgs@gmail.com>
2018-02-01 02:20:24 +0300

    js__cart

--- js/index.js
+++ js/index.js
@@ -11,8 +11,6 @@ class Container {
     this.htmlCode = '';
 
   }
-
-
   render() {
     return this.htmlCode;
   }
@@ -31,7 +29,7 @@ class SearchSelectList extends Container {
     let items = this.items;
     items.forEach(function (item) {
       result += '<li class="' + item.class + '" data-value=' + item.data + ' >' + item.name + '</li>';
-    })
+    });
     /* for( let i = 0, i < this.items.length; i++){
        result += i
      }*/
@@ -78,10 +76,10 @@ class Basket extends Container {
       url: './js/json/basket.json',
       dataType: 'json',
       success: function (data) {
-          let quantity = 0;
-          let b = function(){
+        let quantity = 0;
+        let b = function () {
 
-          for( let i = 0; i < data.basket.length; i++){
+          for (let i = 0; i < data.basket.length; i++) {
             quantity += +data.basket[i]['quantity'];
           }
           return quantity;
@@ -137,33 +135,31 @@ class Basket extends Container {
 
   };
 
-  refresh () {
+  refresh() {
     let $basketItemsDiv = $('#purchase__list');
     $basketItemsDiv.empty();
 
 
-
-
     for (let i = 0; i < this.basketItems.length; i++) {
       $basketItemsDiv.append(
-        '<div class="purchase__item" id="purchase__item-'+ i + '"><div class="purchase__img"><img src="'
+        '<div class="purchase__item" id="purchase__item-' + i + '"><div class="purchase__img"><img src="'
         + this.basketItems[i]["src"]
-        +'"></div><div class="purchase__info"><p class="purchase__title">'
-        +this.basketItems[i]["title"]+'</p>'
-        + '<div id="purchase__reiting'+ i +'" class="purchase__reiting"></div>'
+        + '"></div><div class="purchase__info"><p class="purchase__title">'
+        + this.basketItems[i]["title"] + '</p>'
+        + '<div id="purchase__reiting' + i + '" class="purchase__reiting"></div>'
         + '<span class="purchase__price">'
         + this.basketItems[i]['quantity']
         + ' x $' + this.basketItems[i]['price']
         + '</span></div><div class="purchase__delite" id="basketItemDel-' + i + '">'
-        +'<i class="fa fa-times-circle " aria-hidden="true"></i>' +
+        + '<i class="fa fa-times-circle " aria-hidden="true"></i>' +
         '</div></div>');
 
-      for( let j = 0; j < this.basketItems[i]['stars']; j++) {
-        let star = $('<i />',{
+      for (let j = 0; j < this.basketItems[i]['stars']; j++) {
+        let star = $('<i />', {
           class: "fa fa-star",
           ariaHidden: 'true'
         });
-        $("#purchase__reiting"+ i).append(star);
+        $("#purchase__reiting" + i).append(star);
       }
 
     }
@@ -171,23 +167,20 @@ class Basket extends Container {
     let $basketDataDiv = $('#basket_data');
     $basketDataDiv.empty();
     $basketDataDiv.append('<p class="total-price__text">Всего товаров: ' + this.countGoods + '</p>');
-    $basketDataDiv.append('<p class="total-price__price">Сумма: ' + this.amount + '</p>');
-
-
-
+    $basketDataDiv.append('<p class="total-price__price">Сумма: ' + Math.round(this.amount*100)/100 + '</p>');
 
 
     let deleteItem = this.delete.bind(this);
     $('.purchase__item').on('click', '.purchase__delite', function () {
-        console.log(this);
-        let item = $(this).attr('id').split('-')[1];
-        deleteItem(item);
-        console.log(this);
+      let item = $(this).attr('id').split('-')[1];
+      deleteItem(item);
+      });
+      $('.card__img--text').text(this.countGoods);
 
-    });
 
 
   };
+
   delete(item) {
     let itemToDelete = this.basketItems[item];
     this.countGoods -= itemToDelete['quantity'];
@@ -198,22 +191,56 @@ class Basket extends Container {
 
   };
 
-add(product, quantity, price) {
+  add(product, quantity, price, src, title, stars) {
     // console.log(product, quantity, price);
     let basketItems = {
       "id_product": product,
-      "price": price
+      "price": price,
+      "src": src,
+      "title": title,
+      "quantity": quantity,
+      "stars": stars
+
     };
 
+////////////////////////////////////////
+    //попытался сделать проверку на наличие в корзине товара с таким же id, но кажется запутался..
+    // по идее нужно проверить все this.basketItems[j].id_product на наличие данного id.. и кажется это надо делать не через for а через for in..
+    /*
+    for( let j = 0; j < this.basketItems.length; j++){
+      if(basketItems.id_product == this.basketItems[j].id_product ){
+
+        this.basketItems[j].quantity +=quantity;
+        this.countGoods++;
+        this.amount += +price;
+
+      } else{
+        for (let i = 1; i <= quantity; i++) {
+          this.basketItems.push(basketItems);
+          this.countGoods++;
+          this.amount += +price*quantity;
+        }
+      }
+
+*/
     for (let i = 1; i <= quantity; i++) {
       this.basketItems.push(basketItems);
       this.countGoods++;
-      this.amount += +price;
+      this.amount += +price * quantity;
     }
-
     this.refresh();
-    console.log(this.basketItems);
   };
+
+}
+
+
+class HeaderMenu extends Container{
+    Constructor(my_id, my_class) {
+      this.id = my_id;
+      this.className = my_class;
+      this.items = [];
+     // this.ajax();
+    }
 
 }
 
@@ -221,31 +248,39 @@ add(product, quantity, price) {
 
 
 
-  $(document).ready(function () {
-    let searchList = new SearchSelectList();
-    searchList.ajax();
-    $('.search-select').append();
 
 
-    let basket = new Basket();
-    $('.card__menu').append(basket.collectBasketItems());
-
-    $('.product__btn').on('click', function () {
-      let idProduct = parseInt($(this).attr('id').split('_')[1]);
-      let price = parseInt($(this).parent().find('.product-price').text());
-
-      basket.add(idProduct, 1, price);
-      return false
-    });
-
-
-
-    $('#search-select__list li').on('click', function () {
-      $('#search__select option').attr('value', this.innerHTML).text(this.innerHTML);
-    });
+$(document).ready(function () {
+  let searchList = new SearchSelectList();
+  searchList.ajax();
+  $('.search-select').append(searchList.render());
 
 
 
 
+
+  let basket = new Basket();
+  $('.card__menu').append(basket.collectBasketItems());
+
+  $('.product__btn').on('click', function () {
+    let idProduct = parseInt($(this).attr('id').split('-')[1]);
+    let price = parseFloat($(this).parents('.product__item').find('.product__price').text()
+      .replace(/[^\d.]/g,""));
+    let src = $(this).parents('.product__item').find('.product__img').attr('src');
+    let title = $(this).parents('.product__item').find('.product__text').text();
+    let stars =  parseInt($(this).attr('data-stars')) || 5;
+    let quantity = 1;
+
+
+   basket.add(idProduct, quantity, price, src, title, stars)
+    // console.log(idProduct, quantity, price, src, title, stars);
 
   });
+
+
+  $('#search-select__list li').on('click', function () {
+    $('#search__select option').attr('value', this.innerHTML).text(this.innerHTML);
+  });
+
+
+});
