9192bb2295d086d7c59f72ca236911b643c74217
AnorVen <efremovgs@gmail.com>
2018-02-04 16:04:50 +0300

    header menu

--- js/index.js
+++ js/index.js
@@ -239,13 +239,183 @@ class HeaderMenu extends Container{
       this.id = my_id;
       this.className = my_class;
       this.items = [];
-     // this.ajax();
+      this.ajax();
     }
+    ajax(){
+      $.ajax({
+        url: './js/json/headerMenu.json',
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+          this.id = data.list.id;
+          this.className = data.list.class;
+          let a = [];
+          for (let i = 0; i < data.menuItems.length; i++) {
+            a.push(data.menuItems[i]);
+
+          }
+          this.items = a;
+          this.renderHeaderMenu();
+        },
+        context: this
+      });
+    }
+  renderHeaderMenu() {
+      console.log(123)
+    let result = '<ul class="'+this.className+'" id="'+this.id+'"></ul>';
+  //  for (let i = 0; i < this.items.length; i++) {
+    for (let item in this.items.length) {
+
+      let itemMenu = '<li class="' + item.class + '"><a href="' + item.link + '">' +
+        item.title + '</a></li>';
+/*
+      if (this.items[i].submenu){
+        let submenu = this.renderSubMenu(this.items[i].submenu);
+        console.log(submenu);
+        item.append(submenu);
+      }
+      console.log(item);
+      console.log(result);
+
+    */
+      console.log(itemMenu);
+     result.append(itemMenu);
+
+
+    }
+
+    return result;
+  }
+
+  /*
+  renderSubMenu(){
+    let res = $('<ul />',{
+      class: this.items['subMenu'].class,
+      text: this.items['subMenu'].title
+    });
+
+      for( let i = 0; i < this.items['submenu'].length; i++ ){
+        for( let j = 0; j < this.items['submenu'].list.length; i++){
+          res.append(123)
+
+        }
+      }
+    return res;
+console.log(res)
+  }
+*/
 
 }
 
 
+/*
+class SubMenu extends HeaderMenu{
 
+  Constructor(my_id, my_class, this.items){
+    super(this.items);
+    this.className = my_class;
+  }
+  render() {
+    return '<li>' + Menu.render.call(this) + '</li>';
+  }
+}
+
+class MenuItem extends HeaderMenu{
+  Constructor(my_href, my_name, my_class){
+    this.className = my_class;
+    this.href = my_href;
+    this.name = my_name;
+  }
+
+  render() {
+    return '<li class="'+this.className+'"><a href="'+this.href+'">'+this.title+'</a></li>';
+  }
+}
+*/
+
+
+
+
+
+class Products extends Container{
+  Constructor(my_id, my_class) {
+    this.id = my_id;
+    this.className = my_class;
+    this.items = [];
+    this.ajax();
+  }
+
+  render() {
+
+
+    /*
+   let items =  this.items;
+   items.forEach(function (item) {
+     result.append('<img src="' + item.src +  '" alt="' + item.title + '" class="product__img" width="261" height="280"> <a href="#" class="product__link"><p class="product__text">' + item.title + '</p></a>' +
+       '<p class="product__price">$' + item.price + '</p> ' +
+       ' <div class="product__shadow"><button class="product__btn" id="id_product-' + item.id_item +'">'+
+       '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24">' +
+       '<defs><style> .cls-1 {fill: #fff; stroke: #fff; stroke-linejoin: round; stroke-width: 0px;' +
+       'fill-rule: evenodd;}</style>\</defs><path id="Forma_1_copy" data-name="Forma 1 copy" class="cls-1"' + ' d="M898.851,2004.79h2.438l3.474,13.25a0.862,0.862,0,0,0,.823.65h10.676a0.852,0.852,0,0,0,.78-0.54l3.885-9.41a0.941,0.941,0,0,0-.071-0.85,0.827,0.827,0,0,0-.709-0.41h-11.8a0.9,0.9,0,0,0,0,1.8h10.492l-3.148,7.62h-9.471l-3.474-13.24a0.86,0.86,0,0,0-.822-0.66h-3.077A0.9,0.9,0,0,0,898.851,2004.79Zm5.926,20.21a2.038,2.038,0,1,0-1.928-2.03A1.983,1.983,0,0,0,904.777,2025Zm12.053,0h0.141a1.865,1.865,0,0,0,1.319-.7,2.117,2.117,0,0,0,.468-1.48,1.976,1.976,0,0,0-2.056-1.89,2.015,2.015,0,0,0-1.787,2.17A1.982,1.982,0,0,0,916.83,2025Z" transform="translate(-897 -2002)"/></svg>' +
+     'Add to Cart</button></div>');
+});
+
+ */
+    let result = $('.product');
+    result.empty();
+    if(!result.hasClass('product__list')){
+      result.addClass('product__list')
+    }
+
+
+
+    for(let j = 0; j < this.items.length; j++){
+      let product__item = $('<div />',{
+        class: 'product__item'
+      });
+
+      product__item.append('<img src="' + this.items[j].src +  '" alt="' + this.items[j].title + '" class="product__img" width="261" height="280"> <a href="#" class="product__link"><p class="product__text">' + this.items[j].title + '</p></a>' +
+       '<p class="product__price">$' + this.items[j].price + '</p> ' +
+       ' <div class="product__shadow"><button class="product__btn" id="id_product-' + this.items[j].id_item +'">'+
+       '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24">' +
+       '<defs><style> .cls-1 {fill: #fff; stroke: #fff; stroke-linejoin: round; stroke-width: 0px;' +
+       'fill-rule: evenodd;}</style>\</defs><path id="Forma_1_copy" data-name="Forma 1 copy" class="cls-1"' + ' d="M898.851,2004.79h2.438l3.474,13.25a0.862,0.862,0,0,0,.823.65h10.676a0.852,0.852,0,0,0,.78-0.54l3.885-9.41a0.941,0.941,0,0,0-.071-0.85,0.827,0.827,0,0,0-.709-0.41h-11.8a0.9,0.9,0,0,0,0,1.8h10.492l-3.148,7.62h-9.471l-3.474-13.24a0.86,0.86,0,0,0-.822-0.66h-3.077A0.9,0.9,0,0,0,898.851,2004.79Zm5.926,20.21a2.038,2.038,0,1,0-1.928-2.03A1.983,1.983,0,0,0,904.777,2025Zm12.053,0h0.141a1.865,1.865,0,0,0,1.319-.7,2.117,2.117,0,0,0,.468-1.48,1.976,1.976,0,0,0-2.056-1.89,2.015,2.015,0,0,0-1.787,2.17A1.982,1.982,0,0,0,916.83,2025Z" transform="translate(-897 -2002)"/></svg>' +
+       'Add to Cart</button></div>');
+
+      result.append(product__item);
+
+    }
+    return result;
+
+
+  }
+
+  ajax() {
+    $.ajax({
+      url: './js/json/items.json',
+      dataType: 'json',
+      async: false,
+      success: function (data) {
+        this.id = data.list.id;
+        this.className = data.list.class;
+        let a = [];
+        for (let i = 0; i < data.items.length; i++) {
+          a.push(data.items[i]);
+
+        }
+        this.items = a;
+        /*
+        for(let index in data.menuItems) {
+          this.items.push(index)
+        }
+*/
+        this.render();
+      },
+      context: this
+    });
+  }
+
+}
 
 
 
@@ -254,6 +424,15 @@ $(document).ready(function () {
   let searchList = new SearchSelectList();
   searchList.ajax();
   $('.search-select').append(searchList.render());
+
+
+  let items = new Products();
+  items.ajax();
+
+  let headerMenu = new HeaderMenu();
+  headerMenu.ajax();
+  headerMenu.renderHeaderMenu();
+
 
 
 
